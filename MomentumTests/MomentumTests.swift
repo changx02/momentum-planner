21e8ff4113d3249a911e463511dae97d7db343eb
changx02 <changx02@gmail.com>
2026-02-10 20:09:12 +0800

    Initial Commit

---
+++ MomentumTests/MomentumTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumTests.swift
+//  MomentumTests
+//
+//  Created by Vee Lee on 2026/2/10.
+//
+
+import Testing
+@testable import Momentum
+
+struct MomentumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
